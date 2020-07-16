1a5d925313bc3d1a10a3ffd6277c5e35cebe43bf
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-07-16 12:14:17 +0500

    fixed layouts in these sections.

---
+++ app/src/main/java/edu/aku/hassannaqvi/covid_sero/ui/sections/SectionBActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.covid_sero.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.covid_sero.R;
+
+public class SectionBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_b);
+    }
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/covid_sero/ui/sections/SectionDActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.covid_sero.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.covid_sero.R;
+
+public class SectionDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_d);
+    }
+}
