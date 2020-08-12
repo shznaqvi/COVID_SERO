03a66260aad4a93f5386a15c4ec034dd3d1dec9a
ali.azaz <ali.azaz.alam@hotmail.com>
2020-08-12 18:37:11 +0500

    Commit: *Modified MemberListAdapter.kt

--- app/src/main/java/edu/aku/hassannaqvi/covid_sero/adapter/MemberListAdapter.kt
+++ app/src/main/java/edu/aku/hassannaqvi/covid_sero/adapter/MemberListAdapter.kt
@@ -23,7 +23,7 @@ class MemberListAdapter(private val mContext: Context, private var mList: List<P
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
         holder.bi.parentLayout.tag = i
         holder.bi.name.text = mList[i].memberName
-        holder.bi.age.text = mList[i].agey
+        holder.bi.age.text = String.format("Months: %s & Years:%s", mList[i].agem, mList[i].agey)
         val sb = mList[i].getsB()
         holder.bi.martialstatus.text = if (sb != "") getMaritialStatus(JSONObject(sb).getString("pb03").toInt()) else "N/A"
         if (mList[i].cstatus != "1")
